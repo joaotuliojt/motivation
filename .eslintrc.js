6bdc5b5228d6d200a2e4e2b7167408fd7d1918d1
joaotuliojt <joaotuliosoares@hotmail.com>
2022-07-11 13:57:03 -0300

    chore:eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "airbnb-typescript"],
+  extends: ["plugin:react/recommended", "airbnb", 'airbnb-typescript', "prettier", "plugin:@typescript-eslint/recommended",],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
     ecmaFeatures: {
       jsx: true,
     },
@@ -15,6 +17,10 @@ module.exports = {
   },
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
+    "import/extensions": 1,
+    "import/no-named-as-default": 0,
+    "@typescript-eslint/strict-boolean-expressions": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
     "prettier/prettier": ["error"],
     "import/prefer-default-export": "off",
     "import/extensions": [
