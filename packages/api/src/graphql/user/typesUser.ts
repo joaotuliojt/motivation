c4bb3be1b67e9f0dfa2396103d815b9cf6141698
joaotuliojt <joaotuliosoares@hotmail.com>
2022-07-19 11:48:24 -0300

    feat(user): schema type

---
+++ packages/api/src/graphql/user/typesUser.ts
@@ -0,0 +1,23 @@
+import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
+import { IUser } from "../../models/User";
+
+const typeUser = new GraphQLObjectType<IUser>({
+  name: "User",
+  description: "User model",
+  fields: {
+    email: {
+      type: GraphQLString,
+      description: "Email of user",
+    },
+    name: {
+      type: GraphQLString,
+      description: "User name",
+    },
+    password: {
+      type: GraphQLString,
+    },
+    motivations: {
+      type: GraphQLID,
+    },
+  },
+});
