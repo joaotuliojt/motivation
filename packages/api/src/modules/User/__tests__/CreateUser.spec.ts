b9cb630bd13b70316cdf442505744644f37290d0
joaotuliojt <joaotuliosoares@hotmail.com>
2022-07-24 14:04:16 -0300

    feat: add create user tests

--- packages/api/src/modules/User/__tests__/CreateUser.spec.ts
+++ packages/api/src/modules/User/__tests__/CreateUser.spec.ts
@@ -1,14 +1,14 @@
 import { graphql } from "graphql";
 import { fromGlobalId } from "graphql-relay";
-import { connectMongoose, clearDatabase } from "../../../../test/mongoDb";
+import { connectMongoose, clearDatabase, closeDatabase } from "../../../../test/mongoDb";
 import { schema } from "../../../graphql/schema";
 
 describe("Create User tests", () => {
-  beforeAll(async () => connectMongoose());
-  beforeEach(async () => clearDatabase());
+  beforeAll(async () => await connectMongoose());
+  beforeEach(async () => await clearDatabase())
+  afterAll(async () => await clearDatabase())
 
-  it("should query user", async () => {
-
+  it("should create a user", async () => {
     const query = `
       mutation CREATE_USER($name:String! $email:String! $password:String!){
         CreateUser(input: {
@@ -38,4 +38,43 @@ describe("Create User tests", () => {
     const { user } = result.data?.CreateUser as any;
     expect(user).not.toBeFalsy();
   });
+  beforeEach(async () => clearDatabase())
+
+  it("should throw a error if have equals emails", async () => {
+
+    const query = `
+      mutation CREATE_USER($name:String! $email:String! $password:String!){
+        CreateUser(input: {
+          email:$email,
+          name: $name,
+          password:$password
+        }){
+          ... on CreateUserPayload{
+            user{
+              email
+              name
+              id
+              _id
+            }
+          }
+        }
+      }
+    `;
+
+    const user = {
+      name: "Jhon Doe",
+      email: "jhondoe@email.com",
+      password: "iAmTheBest"
+    }
+
+    const createUserFn = async () => {
+      return await graphql({ schema, source: query, variableValues: user });
+    }
+
+    await createUserFn();
+    const resultWithError = await createUserFn();
+
+    const { errors } = resultWithError;
+    expect(errors[0].message).toEqual("This email is already in use")
+  })
 });
