f2f667fd8d5cd2df48091bc52f6fc69aab2f9fbf
joaotuliojt <joaotuliosoares@hotmail.com>
2022-07-26 19:11:17 -0300

    feat: add get motivations

--- packages/api/src/modules/Motivation/MotivationLoader.ts
+++ packages/api/src/modules/Motivation/MotivationLoader.ts
@@ -5,3 +5,8 @@ export async function getMotivation(id: string) {
   const motivation = await Motivation.findOne({ _id: new Types.ObjectId(id) });
   return motivation;
 }
+
+export async function getMotivations(userId: string) {
+  const motivations = await Motivation.find({ userId });
+  return motivations;
+}
