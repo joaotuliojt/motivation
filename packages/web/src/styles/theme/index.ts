d5b72cb7325ba02b86cf18b3cd5692cdf236e5f2
joaotuliojt <joaotuliosoares@hotmail.com>
2022-07-28 16:35:56 -0300

    feat: add chakra theme

--- packages/web/src/styles/theme/index.ts
+++ packages/web/src/styles/theme/index.ts
@@ -1,5 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({});
+const theme = extendTheme({
+  fonts: {
+    heading: `'Source Serif Pro', serif`,
+    body: `'Albert Sans', sans-serif`,
+  },
+});
 
 export { theme };
